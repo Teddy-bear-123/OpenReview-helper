c9a9b8efdae178d2a4589705b70cc173ccaf94dc
Arun Mallya <arun.mallya@gmail.com>
2024-12-24 19:17:36 -0800

    Make it classy.

--- README.md
+++ README.md
@@ -21,8 +21,9 @@ PASSWORD=<YOUR-PASSWORD>
 Run the script to log into your account, gather submission info, and print it.
 ```bash
 >> python run.py --conf iclr_2025 --headless
-Logging in.
 Opening https://openreview.net/group?id=ICLR.cc/2025/Conference/Area_Chairs
+Logging in.
+Waiting for page to finish loading...
 Logged in.
 Found 14 submissions.
 1, 1234, Discovering the most boopable snoots, 8, 9, 10, 10
@@ -34,12 +35,13 @@ Skip `--headless` if you want to watch it do the web navigation.
 If you want to try it on CVPR 2025, use `--conf cvpr_2025` and pass in the `--skip_reviews` flag
 ```bash
 >> python run.py --conf cvpr_2025 --headless --skip_reviews
-Logging in.
 Opening https://openreview.net/group?id=thecvf.com/CVPR/2025/Conference/Area_Chairs
+Logging in.
+Waiting for page to finish loading...
 Logged in.
 Found 15 submissions.
 1, 1234, To boop or not to boop?
 ...
 15, 214, Ursidae are all you need
 ```
-This is because the reviews for CVPR 2025 are probably not in yet. 
+This is because the reviews for CVPR 2025 are probably not in yet, and the script will hang forever waiting for them to load.
--- run.py
+++ run.py
@@ -6,17 +6,12 @@ import numpy as np
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
-CONF_TO_URL_MAPPING = {
-    "iclr_2025": "https://openreview.net/group?id=ICLR.cc/2025/Conference/Area_Chairs",
-    "cvpr_2025": "https://openreview.net/group?id=thecvf.com/CVPR/2025/Conference/Area_Chairs",
-    # Add new ones here.
-}
+from tqdm import tqdm
 
 
 @dataclass
 class Submission:
+    """Class containing submission details."""
     title: str  # Title.
     sub_id: str  # Paper ID.
     ratings: list[int]  # List of reviewer ratings.
@@ -34,6 +29,119 @@ class Submission:
             f"Var: {np.var(self.ratings):.2f}"
 
 
+class ORAPI:
+    conf_to_url = {
+        "iclr_2025": "https://openreview.net/group?id=ICLR.cc/2025/Conference/Area_Chairs",
+        "cvpr_2025": "https://openreview.net/group?id=thecvf.com/CVPR/2025/Conference/Area_Chairs",
+        # Add new ones here.
+    }
+
+    def __init__(self, conf: str, headless: bool = True):
+        """Initializes the OpenReviewAPI.
+
+        Args:
+        conf (str): Name of the conference.
+        headless (bool): Run without opening a browser window if True.
+        skip_reviews (bool): Don't look for paper reviews if True.
+        """
+        # Create webdriver.
+        options = webdriver.FirefoxOptions()
+        if headless:
+            options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=options)
+
+        self._login(self._get_url(conf))
+
+    def __del__(self):
+        self.driver.quit()
+
+    def _get_url(self, conf: str) -> str:
+        """Get conference Area Chair URL."""
+        if conf in self.conf_to_url:
+            return self.conf_to_url[conf]
+        else:
+            raise ValueError(f"Conf: {conf} not supported.")
+
+    def _login(self, url: str) -> None:
+        # Load username and password.
+        load_dotenv()
+        username = os.environ["USERNAME"]
+        password = os.environ["PASSWORD"]
+
+        # Log in and navigate to url.
+        print(f"Opening {url}")
+        self.driver.get(url)
+        self.driver.find_element(By.ID, "email-input").send_keys(username)
+        self.driver.find_element(By.ID, "password-input").send_keys(password)
+        self.driver.find_element(By.CLASS_NAME, "btn-login").click()
+        print("Logging in.")
+
+        # Wait for page to load, get urls to all papers.
+        print("Waiting for page to finish loading...")
+        while True:
+            urls = self.driver.find_elements(By.XPATH, "//div[@class='note']/h4/a")
+            urls = [url.get_attribute("href") for url in urls]
+            if urls:
+                break
+        print("Logged in.")
+        print(f"Found {len(urls)} submissions.")
+        self.paper_urls = urls
+
+    def load_submission(self, url: str, skip_reviews: bool = False) -> Submission:
+        """Navigate to submission link and parse info.
+
+        Args:
+        driver: Instance of Firefox WebDriver.
+        url (str): URL to submission.
+        skip_reviews (bool): Skip looking for reviews and ratings?
+
+        Returns:
+        Instance of Submission().    
+        """
+
+        # Open url.
+        self.driver.get(url)
+
+        # Get submission title and ID.
+        title = self.driver.find_element(By.CLASS_NAME, "citation_title").text
+        content = self.driver.find_element(
+            By.XPATH, "//div[@class='forum-note']/div[@class='note-content']").text
+        sub_id = content.split("Number:")[1].strip()
+
+        # Get replies.
+        if skip_reviews:
+            replies = []
+        else:
+            while True:
+                # Keep trying until page loads...
+                replies = self.driver.find_element(
+                    By.ID, "forum-replies").find_elements(By.CLASS_NAME, "depth-odd")
+                if replies:
+                    break
+
+        # Get ratings and confidences from each valid rating.
+        ratings, confidences = [], []
+        for reply in replies:
+            content = reply.text
+            rating_start = content.find("Rating: ")
+            if rating_start > 0:
+                confidence_start = content.find("Confidence: ")
+                code_start = content.find("Code Of Conduct: ")
+                rating = int(
+                    content[rating_start:confidence_start].split(":")[1].strip())
+                confidence = int(
+                    content[confidence_start:code_start].split(":")[1].strip())
+                ratings.append(rating)
+                confidences.append(confidence)
+
+        return Submission(title, sub_id, ratings, confidences)
+
+    def load_all_submissions(self, skip_reviews: bool = False):
+        """Get all submission info."""
+        subs = [self.load_submission(paper_url, skip_reviews) for paper_url in tqdm(self.paper_urls)]
+        return subs
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--headless", action="store_true",
@@ -41,99 +149,22 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--skip_reviews", action="store_true",
                         help="Skip reviews? Select if no reviews are in yet.")
     parser.add_argument("--conf", type=str,
-                        default="iclr_2025", choices=list(CONF_TO_URL_MAPPING.keys()))
+                        default="iclr_2025", choices=list(ORAPI.conf_to_url.keys()))
     args = parser.parse_args()
-
-    # Add url.
-    args.url = CONF_TO_URL_MAPPING[args.conf]
-
-    # Load username and password.
-    load_dotenv()
-    args.username = os.environ["USERNAME"]
-    args.password = os.environ["PASSWORD"]
-
     return args
 
 
-def get_paper_info(driver, url: str, skip_reviews: bool = False) -> Submission:
-    """Navigate to submission link and parse info.
+def main() -> None:
+    args = parse_args()
 
-    Args:
-    driver: Instance of Firefox WebDriver.
-    url (str): URL to submission.
-    skip_reviews (bool): Skip looking for reviews and ratings?
+    # Initialize API object and get all info.
+    obj = ORAPI(conf=args.conf, headless=args.headless)
+    subs = obj.load_all_submissions(args.skip_reviews)
 
-    Returns:
-    Instance of Submission().    
-    """
-
-    # Open url.
-    driver.get(url)
-
-    # Get submission title and ID.
-    title = driver.find_element(By.CLASS_NAME, "citation_title").text
-    content = driver.find_element(
-        By.XPATH, "//div[@class='forum-note']/div[@class='note-content']").text
-    sub_id = content.split("Number:")[1].strip()
-
-    # Get replies.
-    if skip_reviews:
-        replies = []
-    else:
-        while True:
-            replies = driver.find_element(
-                By.ID, "forum-replies").find_elements(By.CLASS_NAME, "depth-odd")
-            if replies:
-                break
-
-    # Get ratings and confidences from each valid rating.
-    ratings, confidences = [], []
-    for reply in replies:
-        content = reply.text
-        rating_start = content.find("Rating: ")
-        if rating_start > 0:
-            confidence_start = content.find("Confidence: ")
-            code_start = content.find("Code Of Conduct: ")
-            rating = int(
-                content[rating_start:confidence_start].split(":")[1].strip())
-            confidence = int(
-                content[confidence_start:code_start].split(":")[1].strip())
-            ratings.append(rating)
-            confidences.append(confidence)
-
-    return Submission(title, sub_id, ratings, confidences)
-
-
-def main(args: argparse.Namespace) -> None:
-    # Create driver.
-    options = webdriver.FirefoxOptions()
-    if args.headless:
-        options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
-
-    # Log in and navigate to url.
-    print("Logging in.")
-    print(f"Opening {args.url}")
-    driver.get(args.url)
-    driver.find_element(By.ID, "email-input").send_keys(args.username)
-    driver.find_element(By.ID, "password-input").send_keys(args.password)
-    driver.find_element(By.CLASS_NAME, "btn-login").click()
-    print("Logged in.")
-
-    # Wait for page to load, get urls to all papers.
-    while True:
-        urls = driver.find_elements(By.XPATH, "//div[@class='note']/h4/a")
-        urls = [url.get_attribute("href") for url in urls]
-        if urls:
-            break
-    print(f"Found {len(urls)} submissions.")
-
-    # Visit each url.
-    for idx, url in enumerate(urls):
-        print(f"{idx + 1}, {get_paper_info(driver, url, args.skip_reviews)}")
-
-    driver.quit()
+    # Print info.
+    for idx, sub in enumerate(subs):
+        print(f"{idx + 1}, {sub}")
 
 
 if __name__ == "__main__":
-    main(parse_args())
+    main()
